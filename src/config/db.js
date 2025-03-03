064f94da777dad4a9b875d59dc898e4ebad7a51b
Mobassera Zaman <mobassera.zaman@mail.mcgill.ca>
2025-03-03 17:37:08 -0500

    modified:   src/config/db.js

--- src/config/db.js
+++ src/config/db.js
@@ -5,6 +5,9 @@ const connectDB = async () => {
         mongoose.connection.on("connected", () => {
             console.log(`Connected to MongoDB at ${mongoose.connection.host}`)
         })
+        const MONGODB_URI = process.env.MONGODB_URI;
+        if(MONGODB_URI)  console.log(MONGODB_URI);
+        else console.log("NO URI FOUND")
         await mongoose.connect(process.env.MONGODB_URI);
     }catch(error){
         console.log("MongoDB connection failed: ", error);
